83aaa16576431aa6417abbfbf0b551053e6eddf8
Neil White <neil.white@uq.edu.au>
2023-07-10 14:12:38 +1000

    progress bar

--- server.R
+++ server.R
@@ -29,41 +29,44 @@ server <- function(input, output, session) {
       tables <- getTables()[,1]
       
       files <- vector()
-      for(tabName in tables) {
-        csv <- paste(tabName, 'csv', sep = ".")
-        
-        sensorName <- getSensorName(tabName)
-        
-        if(input$allData) {
-          tmp <- getAllDataForDevice(tabName)
-        } else {
-          #browser() 
-          start <- as.numeric(anytime(input$dateRange[1])) - 36000
-          end <- as.numeric(anytime(input$dateRange[2])) - 36000
-          tmp <- getDataRangeForDevice(tabName, start, end)
+      withProgress(message = 'Zipping files', value = 0, {
+        N <- length(tables)
+        for(tabName in tables) {
+          csv <- paste(tabName, 'csv', sep = ".")
+          
+          sensorName <- getSensorName(tabName)
+          
+          if(input$allData) {
+            tmp <- getAllDataForDevice(tabName)
+          } else {
+            #browser() 
+            start <- as.numeric(anytime(input$dateRange[1])) - 36000
+            end <- as.numeric(anytime(input$dateRange[2])) - 36000
+            tmp <- getDataRangeForDevice(tabName, start, end)
+          }
+          
+          AEST <- iso8601(anytime(tmp$timestamp))
+          results <- cbind(tmp$timestamp, AEST, tmp$value)
+          colnames(results) <- c("UnixTimestamp", "AEST", "Value")
+          
+          write.csv(results, file.path(td, 'tmp.csv'), row.names=F)
+          
+          csvFile <- file(file.path(td,'tmp.csv'), "r")
+          theCSV <- readLines(csvFile)
+          close(csvFile)
+          
+          csvFile <- file(csv, "w")
+          write(sensorName, csv)
+          close(csvFile)
+          csvFile <- file(csv, "a")
+          writeLines(theCSV, csvFile)
+          close(csvFile)
+          
+          files <- c(files, csv)
+          incProgress(1/N, detail = paste("Doing file", i))
         }
-        
-        AEST <- iso8601(anytime(1688961568))
-        results <- cbind(tmp$timestamp, AEST, tmp$value)
-        colnames(results) <- c("UnixTimestamp", "AEST", "Value")
-        
-        write.csv(results, file.path(td, 'tmp.csv'), row.names=F)
-        
-        csvFile <- file(file.path(td,'tmp.csv'), "r")
-        theCSV <- readLines(csvFile)
-        close(csvFile)
-        
-        csvFile <- file(csv, "w")
-        write(sensorName, csv)
-        close(csvFile)
-        csvFile <- file(csv, "a")
-        writeLines(theCSV, csvFile)
-        close(csvFile)
-        
-        files <- c(files, csv)
-        print(csv)
-      }
-      zip(file, files)
+      })
+        zip(file, files)
     }
   )
   
